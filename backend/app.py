f4eae31bd9bb9238ea46f9bc2d120faba01aae31
PRASAD PANIGRAHY <prasadpanigrahy.ccschool@gmail.com>
2025-06-29 13:33:13 +0530

    Deploy-ready for Render

--- backend/app.py
+++ backend/app.py
@@ -120,7 +120,5 @@ def clear_chat():
 
 # Start app
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
